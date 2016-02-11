bd3c04992d6600f2549859e2a6545c38309e2545
ravenusmc <mcuddy77@gmail.com>
2016-02-11 14:58:59 -0500

    finishing up with user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211193425) do
+ActiveRecord::Schema.define(version: 20160211195031) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160211193425) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
