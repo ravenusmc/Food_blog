0a4168a065760fc6eb5f38693cac3f620123f55f
ravenusmc <mcuddy77@gmail.com>
2016-02-13 10:53:34 -0500

    worked on routes and readme file

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
+  get 'your_posts' => 'blog_posts#your_posts'
+
   devise_for :users
   resources :comments
   resources :blog_posts
 
-  get 'your_posts' => 'blog_posts#your_posts'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
